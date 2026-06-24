ee81ef663d390d87a95f0c78b1e61cb07bab128e
“Andrew <xhinker@hotmail.com>
2026-06-24 16:19:38 -0700

    remove text normalizer

--- src/az_voice/utils/__init__.py
+++ src/az_voice/utils/__init__.py
@@ -1,11 +1,9 @@
 """Shared utilities for text and audio processing."""
 
-from az_voice.utils.text_normalizer import _normalize_cjk_spaces
 from az_voice.utils.text_chunker import split_text_for_tts
 from az_voice.utils.audio_utils import concatenate_wavs
 
 __all__ = [
     "split_text_for_tts",
     "concatenate_wavs",
-    "_normalize_cjk_spaces",
 ]
